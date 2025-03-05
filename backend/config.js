10305290adae7f72d2b630babeb7d048636a48a1
Abhiram <b.abhi2277@gmail.com>
2025-03-05 11:32:51 +0530

    add nodemon

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 3000;
-// export const DB_URL = "mongodb://localhost:27017/accounts";
 export const DB_URL =
 	"mongodb+srv://XPUser:XPUserPWD@xptrack.bpsuq.mongodb.net/?retryWrites=true&w=majority&appName=XPTrack";
+// export const DB_URL = "mongodb://localhost:27017/accounts";
