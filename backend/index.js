bd8d7e208b9b240d9726eddc795d0996ba42bc5a
Abhiram <b.abhi2277@gmail.com>
2025-12-08 12:53:08 +0530

    Add mongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://xpense-track-web.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
